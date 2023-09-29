eeb6a63c63ffe023bf8d88e92aa861a1d0369f4c
Shamel Hok <Shamel.Hok@abstract-tech.com>
2023-09-29 16:16:20 +0100

    baseurl const fix

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const { PORT = 9090 } = process.env;
 
-const baseUrl = `http://localhost:${PORT}` 
+let baseUrl = `http://localhost:${PORT}` 
 if(process.env.NODE_ENV =="production"){
  baseUrl= "https://hubspot-tinkle-poc.onrender.com"
 }
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // require('dotenv').config();
-console.log("in index.js");
 const { app } = require("./app");
 const opn = require('open');
 
 const { PORT, baseUrl } = require("./constants");
 
 app.listen(PORT, () => console.log(`=== Starting your app on ${baseUrl} ===`));
-console.log(process.env,process.env.NODE_ENV);
 //if (process.env.NODE_ENV "development") {
     opn(baseUrl);
 //}

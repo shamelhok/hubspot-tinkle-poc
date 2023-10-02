7738fde3a3a3e95efdea7e4ef042e1daf0304f6a
Shamel Hok <Shamel.Hok@abstract-tech.com>
2023-10-02 15:48:19 +0100

    crm actions buttons function

--- CRMControllers.js
+++ CRMControllers.js
@@ -1,5 +1,6 @@
 const request = require('request-promise-native');
 exports.crmSampleController = async(req, res) => {
+    
     res.setHeader('Content-Type', 'application/json');
     const result = {
         "results": [
@@ -16,6 +17,100 @@ exports.crmSampleController = async(req, res) => {
                 "status": "In Progress",
                 "ticket_type": "Bug",
                 "updated": "2016-09-28",
+                "properties": [
+                    {
+                        "label": "Resolved by",
+                        "dataType": "EMAIL",
+                        "value": "ijones@hubspot.com"
+                    },
+                    {
+                        "label": "Resolution type",
+                        "dataType": "STRING",
+                        "value": "Referred to documentation"
+                    },
+                    {
+                        "label": "Resolution impact",
+                        "dataType": "CURRENCY",
+                        "value": "94.34",
+                        "currencyCode": "GBP"
+                    }
+                ],
+                "actions": [
+                    {
+                        "type": "IFRAME",
+                        "width": 890,
+                        "height": 748,
+                        "uri": "https://hubspot-tinkle-poc.onrender.com/",
+                        "label": "popup",
+                        "associatedObjectProperties": []
+                    },
+                    {
+                        "type": "ACTION_HOOK",
+                        "httpMethod": "GET",
+                        "associatedObjectProperties": [],
+                        "uri": "https://nc-news-api-rwfo.onrender.com/api",
+                        "label": "action"
+                    },
+                    {
+                        "type": "CONFIRMATION_ACTION_HOOK",
+                        "confirmationMessage": "Are you sure you want to delete this ticket?",
+                        "confirmButtonText": "Yes",
+                        "cancelButtonText": "No",
+                        "httpMethod": "GET",
+                        "associatedObjectProperties": [
+                            "protected_account"
+                        ],
+                        "uri": "https://nc-news-api-rwfo.onrender.com/api",
+                        "label": "confirm action"
+                    }
+                ]
+            }
+        ],
+        "settingsAction": {
+            "type": "IFRAME",
+            "width": 890,
+            "height": 748,
+            "uri": "https://example.com/settings-iframe-contents",
+            "label": "Settings"
+        },
+        "primaryAction": {
+            "type": "IFRAME",
+            "width": 890,
+            "height": 748,
+            "uri": "https://cdne-jupiter-dev-uks-01.azureedge.net/",
+            "label": "Open Web app"
+        }
+    }
+    const card1 = result.results[0]
+    try{
+
+        const response=await request.get("https://api-jupiter-dev-uks-01.azurewebsites.net/api/tier/all",
+        {
+            headers:{
+      'X-API-KEY':"436efa91-5a02-4305-b8d5-57d0ab0d2bd6"
+            }
+        })
+        const body = JSON.parse(response)
+        const tiers = body.data
+        card1.properties.push(
+            { 
+            "label": "tier name",
+            "dataType": "STRING",
+            "value": tiers[0].tierName
+    })
+    }catch(err){
+        console.log(err);
+    }
+    res.send(result)
+}
+
+exports.crmNcController= async(req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+    const result = {
+        "results": [
+            {
+                "objectId": 1,
+                "title": "API sample",
                 "properties": [
                     {
                         "label": "Resolved by",
@@ -71,113 +166,7 @@ exports.crmSampleController = async(req, res) => {
                         "label": "Delete"
                     }
                 ]
-            },
-            {
-                "objectId": 988,
-                "title": "API-54: Question about bulk APIs",
-                // "link": "http://example.com/2",
-                "created": "2016-08-04",
-                "priority": "LOW",
-                "project": "API",
-                "reported_by": "ksmith@hubspot.com",
-                "description": "Customer is not able to find documentation about our bulk Contacts APIs.",
-                "reporter_type": "Support Rep",
-                "status": "Resolved",
-                "ticket_type": "Bug",
-                "updated": "2016-09-23",
-                "properties": [
-                    {
-                        "label": "Resolved by",
-                        "dataType": "EMAIL",
-                        "value": "ijones@hubspot.com"
-                    },
-                    {
-                        "label": "Resolution type",
-                        "dataType": "STRING",
-                        "value": "Referred to documentation"
-                    },
-                    {
-                        "label": "Resolution impact",
-                        "dataType": "CURRENCY",
-                        "value": "94.34",
-                        "currencyCode": "GBP"
-                    }
-                ],
-                "actions": [
-                    {
-                        "type": "IFRAME",
-                        "width": 890,
-                        "height": 748,
-                        "uri": "https://example.com/edit-iframe-contents",
-                        "label": "Edit"
-                    },
-                    {
-                        "type": "CONFIRMATION_ACTION_HOOK",
-                        "confirmationMessage": "Are you sure you want to delete this ticket?",
-                        "confirmButtonText": "Yes",
-                        "cancelButtonText": "No",
-                        "httpMethod": "DELETE",
-                        "associatedObjectProperties": [
-                            "protected_account"
-                        ],
-                        "uri": "https://example.com/tickets/245",
-                        "label": "Delete"
-                    }
-                ]
-            },
-            // {
-            //     "objectId": 988,
-            //     "title": "Card 3",
-            //     "link": "http://example.com/2",
-            //     "created": "2016-08-04",
-            //     "priority": "HIGH",
-            //     "project": "API",
-            //     "reported_by": "ksmith@hubspot.com",
-            //     "description": "Customer is not able to find documentation about our bulk Contacts APIs.",
-            //     "reporter_type": "Support Rep",
-            //     "status": "Resolved",
-            //     "ticket_type": "Bug",
-            //     "updated": "2016-09-23",
-            //     "properties": [
-            //         {
-            //             "label": "Resolved by",
-            //             "dataType": "EMAIL",
-            //             "value": "ijones@hubspot.com"
-            //         },
-            //         {
-            //             "label": "Resolution type",
-            //             "dataType": "STRING",
-            //             "value": "Referred to documentation"
-            //         },
-            //         {
-            //             "label": "Resolution impact",
-            //             "dataType": "CURRENCY",
-            //             "value": "94.34",
-            //             "currencyCode": "GBP"
-            //         }
-            //     ],
-            //     "actions": [
-            //         {
-            //             "type": "IFRAME",
-            //             "width": 890,
-            //             "height": 748,
-            //             "uri": "https://tools.hubteam.com/integrations-iframe-test-app",
-            //             "label": "Edit"
-            //         },
-            //         {
-            //             "type": "CONFIRMATION_ACTION_HOOK",
-            //             "confirmationMessage": "Are you sure you want to delete this ticket?",
-            //             "confirmButtonText": "Yes",
-            //             "cancelButtonText": "No",
-            //             "httpMethod": "DELETE",
-            //             "associatedObjectProperties": [
-            //                 "protected_account"
-            //             ],
-            //             "uri": "https://api.hubapi.com/linked-sales-objects-test-application/v1/actions/demo-ticket/988",
-            //             "label": "Delete"
-            //         }
-            //     ]
-            // }
+            }
         ],
         "settingsAction": {
             "type": "IFRAME",
@@ -190,30 +179,14 @@ exports.crmSampleController = async(req, res) => {
             "type": "IFRAME",
             "width": 890,
             "height": 748,
-            "uri": "https://cdne-jupiter-dev-uks-01.azureedge.net/",
+            "uri": "https://nc-news-shamel.netlify.app/",
             "label": "Open Web app"
         }
     }
-    const card1 = result.results[0]
-    try{
-
-        const response=await request.get("https://api-jupiter-dev-uks-01.azurewebsites.net/api/tier/all",
-        {
-            headers:{
-      'X-API-KEY':"436efa91-5a02-4305-b8d5-57d0ab0d2bd6"
-            }
-        })
-        const body = JSON.parse(response)
-        const tiers = body.data
-        card1.properties.push(
-            { 
-            "label": "Resolution type",
-            "dataType": "STRING",
-            "value": tiers[0].tierName
-    })
-        console.log(card1);
-    }catch(err){
-        console.log(err);
-    }
-    res.send(result)
+    const user={
+        username: 'tickle122',
+        name: 'Tom Tickle',
+        avatar_url:
+          'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953'
+      }
 }

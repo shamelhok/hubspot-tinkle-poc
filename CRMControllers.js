426f3b57ae52943811deef1b24012de1f8851787
Shamel Hok <Shamel.Hok@abstract-tech.com>
2023-10-02 10:43:50 +0100

    3rd card

--- CRMControllers.js
+++ CRMControllers.js
@@ -106,7 +106,60 @@ exports.crmSampleController = (req, res) => {
                         "label": "Delete"
                     }
                 ]
-            }
+            },
+            {
+                "objectId": 988,
+                "title": "API-54: Question about bulk APIs",
+                "link": "http://example.com/2",
+                "created": "2016-08-04",
+                "priority": "HIGH",
+                "project": "API",
+                "reported_by": "ksmith@hubspot.com",
+                "description": "Customer is not able to find documentation about our bulk Contacts APIs.",
+                "reporter_type": "Support Rep",
+                "status": "Resolved",
+                "ticket_type": "Bug",
+                "updated": "2016-09-23",
+                "properties": [
+                  {
+                    "label": "Resolved by",
+                    "dataType": "EMAIL",
+                    "value": "ijones@hubspot.com"
+                  },
+                  {
+                    "label": "Resolution type",
+                    "dataType": "STRING",
+                    "value": "Referred to documentation"
+                  },
+                  {
+                    "label": "Resolution impact",
+                    "dataType": "CURRENCY",
+                    "value": "94.34",
+                    "currencyCode": "GBP"
+                  }
+                ],
+                "actions": [
+                  {
+                    "type": "IFRAME",
+                    "width": 890,
+                    "height": 748,
+                    "uri": "https://tools.hubteam.com/integrations-iframe-test-app",
+                    "label": "Edit"
+                  },
+                  {
+                    "type": "CONFIRMATION_ACTION_HOOK",
+                    "confirmationMessage": "Are you sure you want to delete this ticket?",
+                    "confirmButtonText": "Yes",
+                    "cancelButtonText": "No",
+                    "httpMethod": "DELETE",
+                    "associatedObjectProperties": [
+                      "protected_account"
+                    ],
+                    "uri": "https://api.hubapi.com/linked-sales-objects-test-application/v1/actions/demo-ticket/988",
+                    "label": "Delete"
+                  }
+                ]
+              }
         ],
         "settingsAction": {
             "type": "IFRAME",
